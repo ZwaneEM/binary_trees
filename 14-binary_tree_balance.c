551020ed64127d679f814088c3480caea9c913ae
Zwaneem <zwaneem68@outlook.com>
2024-02-28 13:22:09 +0200

    all

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,7 +8,7 @@
  * Return: 0 if tree is NULL, otherwise the results
 */
 int binary_tree_balance(const binary_tree_t *tree)
-{	
+{
 	if (tree)
 	{
 		return (height(tree->left) - height(tree->right));
