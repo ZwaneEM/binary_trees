ae693e2df1b484335277df15172c4205aca34eda
Zwaneem <zwaneem68@outlook.com>
2024-02-28 13:21:01 +0200

    all

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,21 +8,33 @@
  * Return: 0 if tree is NULL, otherwise the results
 */
 int binary_tree_balance(const binary_tree_t *tree)
-{
-	int left = 0;
-	int right = 0;
-
-	if (!tree)
-		return (0);
-
-	else
+{	
+	if (tree)
 	{
-		if (tree->left)
-			left = 1 + binary_tree_balance(tree->left);
-
-		if (tree->right)
-			right = 1 + binary_tree_balance(tree->right);
+		return (height(tree->left) - height(tree->right));
 	}
 
-	return (left - right);
+	return (0);
 }
+
+/**
+ * height - measures the height of the tree
+ * @tree: The root node to measure
+ * Return: The height of the tree
+ */
+int height(const binary_tree_t *tree)
+{
+	int l = 0;
+	int r = 0;
+
+	if (tree)
+	{
+		r = ((tree->left) ? 1 + height(tree->right) : 1);
+
+		l = ((tree->right) ? 1 + height(tree->left) : 1);
+
+		return ((l > r) ? l : r);
+	}
+	return (0);
+}
+
