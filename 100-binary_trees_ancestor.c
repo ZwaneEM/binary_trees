7466d3981729a94ef81956856fff5bee690bd122
Zwaneem <zwaneem68@outlook.com>
2024-02-29 14:02:59 +0200

    all

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,13 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestors - finds the lowest common ancestor of two nodes
+ * binary_trees_ancestor - finds the lowest
+ * common ancestor of two nodes
  * @first: is a pointer to the first node
  * @second: is a pointer to the second node
  * Return: a pointer to the lowest common ancestor node of the two/
  * NULL if no common ancestor was found
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
 	binary_tree_t *mom, *pop;
 
